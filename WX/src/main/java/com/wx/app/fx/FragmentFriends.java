9badaa1e8c62421123375309c251f40cd667eaec
darren <darrenfoung@gmail.com>
2016-02-02 21:45:18 +0800

    更新

--- WX/src/main/java/com/wx/app/fx/FragmentFriends.java
+++ WX/src/main/java/com/wx/app/fx/FragmentFriends.java
@@ -1,20 +1,32 @@
 package com.wx.app.fx;
 
-import com.wx.app.R;
-import com.wx.app.domain.User;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.easemob.chat.EMContactManager;
+import com.wx.app.Constant;
+import com.wx.app.R;
+import com.wx.app.WeixinApplication;
+import com.wx.app.domain.User;
+import com.wx.app.fx.others.ContactAdapter;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class FragmentFriends extends Fragment{
 
 	private List<User> contactList;
+	private ListView listView;
+	private List<String> blackList;
+	private ContactAdapter adapter;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,9 +37,24 @@ public class FragmentFriends extends Fragment{
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		contactList = new ArrayList<User>();
+		listView = (ListView) getView().findViewById(R.id.list);
 
+		// 黑名单列表
+		blackList = EMContactManager.getInstance().getBlackListUsernames();
+		contactList = new ArrayList<User>();
+		// 获取设置contactlist
 		getContactList();
+
+		LayoutInflater inflater = LayoutInflater.from(getActivity());
+		View headerView = inflater.inflate(R.layout.item_contact_list_header, null);
+		View footerView = inflater.inflate(R.layout. item_contact_list_footer,null);
+		listView.addHeaderView(headerView);
+		listView.addFooterView(footerView);
+
+		// 设置adapter
+		adapter = new ContactAdapter(getActivity(), R.layout.item_contact_list,
+				contactList);
+		listView.setAdapter(adapter);
 	}
 
 	/*
@@ -36,5 +63,41 @@ public class FragmentFriends extends Fragment{
 	private void getContactList() {
 		contactList.clear();
 		//TODO 获取本地好友资料
+		Map<String, User> userMap = WeixinApplication.getInstance().getContactList();
+		Set<Map.Entry<String, User>> userEntry = userMap.entrySet();
+		Iterator<Map.Entry<String, User>> iterator = userEntry.iterator();
+		while(iterator.hasNext()){
+			Map.Entry<String, User> entry = iterator.next();
+			String key = entry.getKey();
+			if(!Constant.NEW_FRIENDS_USERNAME.equals(key)
+					&& !Constant.GROUP_USERNAME.equals(key)
+					&& !blackList.contains(key)){
+				contactList.add(entry.getValue());
+			}
+		}
+		Collections.sort(contactList,new PinyinComparator());
+	}
+
+	private class PinyinComparator implements java.util.Comparator<User> {
+		@Override
+		public int compare(User lhs, User rhs) {
+			String headerl = lhs.getHeader();
+			String header2 = rhs.getHeader();
+			if(isEmpty(headerl) && isEmpty(header2)){
+				return 0;
+			}
+			if(isEmpty(headerl) ){
+				return -1;
+			}
+			if(isEmpty(header2)){
+				return 1;
+			}
+			String char1 = headerl.substring(0,1).toUpperCase();
+			String char2 = header2.substring(0,1).toUpperCase();
+			return char1.compareTo(char2);
+		}
+		private boolean isEmpty(String str){
+			return "".equals(str.trim());
+		}
 	}
 }
--- WX/src/main/java/com/wx/app/fx/LoginActivity.java
+++ WX/src/main/java/com/wx/app/fx/LoginActivity.java
@@ -1,27 +1,11 @@
 package com.wx.app.fx;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONException;
-import com.alibaba.fastjson.JSONObject;
-import com.easemob.EMCallBack;
-import com.easemob.chat.EMChatManager;
-import com.easemob.exceptions.EaseMobException;
-import com.wx.app.Constant;
-import com.wx.app.R;
-import com.wx.app.WeixinApplication;
-import com.wx.app.fx.others.LoadDataFromServer;
-import com.wx.app.fx.others.LoadDataFromServer.DataCallback;
-import com.wx.app.fx.others.LocalUserInfo;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
@@ -29,6 +13,29 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import com.easemob.EMCallBack;
+import com.easemob.chat.EMChatManager;
+import com.easemob.chat.EMContactManager;
+import com.easemob.chat.EMGroupManager;
+import com.easemob.exceptions.EaseMobException;
+import com.easemob.util.HanziToPinyin;
+import com.wx.app.Constant;
+import com.wx.app.R;
+import com.wx.app.WeixinApplication;
+import com.wx.app.domain.User;
+import com.wx.app.domain.UserDao;
+import com.wx.app.fx.others.LoadDataFromServer;
+import com.wx.app.fx.others.LoadDataFromServer.DataCallback;
+import com.wx.app.fx.others.LocalUserInfo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LoginActivity extends FragmentActivity {
 
@@ -138,7 +145,7 @@ public class LoginActivity extends FragmentActivity {
 
     protected void signIn(final JSONObject json) {
 
-        Log.d(TAG, json.toJSONString());
+        Log.d(TAG, "signIn="+json.toJSONString());
         final String nick = json.getString("nick");
         final String hxid = json.getString("hxid");
         final String password = json.getString("password");
@@ -166,11 +173,19 @@ public class LoginActivity extends FragmentActivity {
             @Override
             public void onSuccess() {
                 try {
+                    // 登陆成功，保存用户名密码
                     WeixinApplication.getInstance().setUserName(hxid);
                     WeixinApplication.getInstance().setPassword(password);
 
-                    // TODO 处理好友和群组
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            landingDialog.setMessage(getString(R.string.list_is_for));
+                        }
+                    });
+                    // TODO 处理好友和群组,怎么运作的？？
+                    EMGroupManager.getInstance().loadAllGroups();
                     EMChatManager.getInstance().loadAllConversations();
+
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -206,8 +221,8 @@ public class LoginActivity extends FragmentActivity {
     private void processContactsAndGroups(final JSONObject json) {
         // todo demo中简单的处理成每次登陆都去获取好友username，开发者自己根据情况而定
         try {
+            //根据EMC返回的好友数据，申请服务器查询好友资料
             List<String> usernames = EMChatManager.getInstance().getContactUserNames();
-            //有好友数据
             if (usernames != null && usernames.size() > 0) {
                 String totaluser = usernames.get(0);
                 for (int i = 1; i < usernames.size(); i++) {
@@ -254,7 +269,121 @@ public class LoginActivity extends FragmentActivity {
     }
 
     private void saveFriends(JSONArray jsonArray) {
-// TODO: 2016/1/28 保存好友信息 
+        // TODO: 2016/1/28 保存好友信息
+        Log.d(TAG, jsonArray.toJSONString());
+
+        Map<String, User>  map = new HashMap<String, User>();
+        if(jsonArray != null){
+            for(int i=0; i < jsonArray.size(); i++){
+                //{"sign":"0","region":"0","hxid":"11236867","acount":"0.00",
+                // "time":"2016-01-24 11:26:56","sex":"0","nick":"erer","fxid":"0",
+                // "tel":"1502","money":"10.00","avatar":"0","password":"1502fei","paypw":""},
+                JSONObject object = jsonArray.getJSONObject(i);
+                String hxid = object.getString("hxid");
+                String fxid = object.getString("fxid");
+                String nick = object.getString("nick");
+                String avatar = object.getString("avatar");
+                String sex = object.getString("sex");
+                String region = object.getString("region");
+                String sign = object.getString("sign");
+                String tel = object.getString("tel");
+
+                User user = new User();
+                user.setFxid(fxid);
+                user.setUsername(hxid);
+                user.setBeizhu("");
+                user.setNick(nick);
+                user.setRegion(region);
+                user.setSex(sex);
+                user.setTel(tel);
+                user.setSign(sign);
+                user.setAvatar(avatar);
+                setUserHearder(hxid, user);
+                map.put(hxid, user);
+            }
+        }
+        // TODO: 2016/2/2 catch
+
+        // 添加user"申请与通知"
+        User newFriends = new User();
+        newFriends.setUsername(Constant.NEW_FRIENDS_USERNAME);
+        String strChat = getResources().getString(
+                R.string.Application_and_notify);
+        newFriends.setNick(strChat);
+        newFriends.setBeizhu("");
+        newFriends.setFxid("");
+        newFriends.setHeader("");
+        newFriends.setRegion("");
+        newFriends.setSex("");
+        newFriends.setTel("");
+        newFriends.setSign("");
+        newFriends.setAvatar("");
+        map.put(Constant.NEW_FRIENDS_USERNAME, newFriends);
+        // 添加"群聊"
+        User groupUser = new User();
+        String strGroup = getResources().getString(R.string.group_chat);
+        groupUser.setUsername(Constant.GROUP_USERNAME);
+        groupUser.setNick(strGroup);
+        groupUser.setHeader("");
+        groupUser.setNick(strChat);
+        groupUser.setBeizhu("");
+        groupUser.setFxid("");
+        groupUser.setHeader("");
+        groupUser.setRegion("");
+        groupUser.setSex("");
+        groupUser.setTel("");
+        groupUser.setSign("");
+        groupUser.setAvatar("");
+        map.put(Constant.GROUP_USERNAME, groupUser);
+        // 存入内存
+        WeixinApplication.getInstance().setContactList(map);
+        // 存入db
+        UserDao userDao = new UserDao(LoginActivity.this);
+        List<User> users = new ArrayList<User>(map.values());
+        userDao.saveContactList(users);
+
+        // 获取黑名单列表
+        try {
+            List<String> blackList = EMContactManager.getInstance()
+                    .getBlackListUsernamesFromServer();
+            EMContactManager.getInstance().saveBlackList(blackList);
+
+            // 获取群聊列表(群聊里只有groupid和groupname等简单信息，不包含members),
+            // sdk会把群组存入到内存和db中
+            EMGroupManager.getInstance().getGroupsFromServer();
+//            addContact("11223354");
+
+        } catch (EaseMobException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*设置hearder属性，方便通讯中对联系人按header分类显示，
+    以及通过右侧ABCD...字母栏快速定位联系人*/
+    private void setUserHearder(String hxid, User user) {
+        String headerName = null;
+        if(!TextUtils.isEmpty(user.getNick())){
+            headerName = user.getNick();
+        }else{
+            headerName = user.getUsername();
+        }
+        headerName = headerName.trim();
+
+        if(hxid.equals(Constant.NEW_FRIENDS_USERNAME)){
+            user.setHeader("");  //新朋友
+        }else if(Character.isDigit(headerName.charAt(0))){
+            user.setHeader("#"); //数字
+        }else{
+            //headerName的首字母英文大写
+            String upperPinyin = HanziToPinyin.getInstance().get(headerName.substring(0,1))
+                    .get(0).target.substring(0,1).toUpperCase();
+            user.setHeader(upperPinyin);
+
+//            char header = user.getHeader().toLowerCase().charAt(0);
+//            if (header < 'a' || header > 'z') {
+//                user.setHeader("#");
+//            }
+        }
     }
 
     private void saveMyInfo(JSONObject json) {
